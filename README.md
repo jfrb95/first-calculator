5d288fd59fadbf935591df850a6a0ad1ff965013
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-13 13:19:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A calculator with basic functions that I created without instructions.
+
+This is the final project of The Odin Project's foundation course.
+Eveything that went into making was done entirely by my own abilities.
---
+++ calculator.js
@@ -0,0 +1,26 @@
+const GLOBAL = (function(){
+    let num1 = 0;
+    let num2 = 0;
+    let operator;
+
+    function add(num1, num2) {
+
+    }
+
+    function subtract(num1, num2) {
+
+    }
+
+    function multiply(num1, num2) {
+
+    }
+
+    function divide(num1, num2) {
+        
+    }
+
+
+    return {
+
+    }
+}());
