101e9652e82db57386089cb0a5003811eddd0b0e
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-13 14:46:27 +0100

    add more functions. Begin changeVariables function
    changeVariables should change the stored num1, num2, and operator
    variables ready to be used in the operate() function. Currently it
    updates the variables and logs onto the console. Current implementation
    may not be final, as it relies on the text content of the buttons in the
    HTML.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,62 @@
 const GLOBAL = (function(){
     const display = document.querySelector(".display");
-    let num1 = 0;
-    let num2 = 0;
-    let operator;
+    const buttons = document.querySelector(".buttons");
+    buttons.addEventListener("click", changeVariable);
+    let num1 = "";
+    let num2 = "";
+    let operator = null;
     let displayValue = 0;
+
+    function changeVariable(event) {
+        const target = event.target;
+        if (target.classList.contains("number")) {
+            if (!operator) {
+                addCharToNum1(target.textContent);
+            } else {
+                addCharToNum2(target.textContent);
+            }
+            console.log("num1:", num1, "num2:", num2);
+        } else if (target.classList.contains("operator")) {
+            
+            switch (target.textContent) {
+                case "+":
+                    operator = add;
+                    console.log("add");
+                    break;
+                case "-":
+                    operator = subtract;
+                    console.log("subtract");
+                    break;
+                case "x":
+                    operator = multiply;
+                    console.log("multiply");
+                    break;
+                case "/":
+                    operator = divide;
+                    console.log("divide");
+                    break;
+                default:
+                    return new Error("Invalid operator");
+            }
+
+        }
+    }
+
+
+    function operate() {
+        if (operator) {
+            operator(Number(num1), Number(num2));
+        }
+    }
+
+    function setOperator(f) {
+        operator = f;
+    }
+
+    function addCharToNum1(num) {num1 = num1.concat(num);}
+
+    function addCharToNum2(num) {num2 = num2.concat(num);}
+
 
     function add(num1, num2) {return num1 + num2}
 
@@ -20,12 +73,14 @@ const GLOBAL = (function(){
     function updateDisplay(str) {
         const text = document.createTextNode(str);
         display.appendChild(text);
+        updateDisplayValue()
     }
 
-    function enterDigit(str) {
-        updateDisplay(str);
-        updateDisplayValue();
+    function populateDisplay(str) {
+        display.textContent = str;
+        updateDisplayValue()
     }
+
 
     return {}
 }());
