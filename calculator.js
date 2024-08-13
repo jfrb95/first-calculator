cfef54b00e3b92791e8c6580099a4e6ac7d76939
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-13 13:54:31 +0100

    add functions to display button presses

--- calculator.js
+++ calculator.js
@@ -1,26 +1,31 @@
 const GLOBAL = (function(){
+    const display = document.querySelector(".display");
     let num1 = 0;
     let num2 = 0;
     let operator;
+    let displayValue = 0;
 
-    function add(num1, num2) {
-        return num1 + num2
+    function add(num1, num2) {return num1 + num2}
+
+    function subtract(num1, num2) {return num1 - num2}
+
+    function multiply(num1, num2) {return num1 * num2}
+
+    function divide(num1, num2) {return num1 / num2}
+
+    function updateDisplayValue() {
+        displayValue = display.textContent;
     }
 
-    function subtract(num1, num2) {
-        return num1 - num2
+    function updateDisplay(str) {
+        const text = document.createTextNode(str);
+        display.appendChild(text);
     }
 
-    function multiply(num1, num2) {
-        return num1 * num2
+    function enterDigit(str) {
+        updateDisplay(str);
+        updateDisplayValue();
     }
 
-    function divide(num1, num2) {
-        return num1 / num2
-    }
-
-
-    return {
-
-    }
+    return {}
 }());
